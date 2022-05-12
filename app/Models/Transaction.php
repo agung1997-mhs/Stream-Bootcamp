6d9b1a5bc630893013246aaf666852656e1adfb3
agung1997-mhs <agungkemayoran97@gmail.com>
2022-05-12 11:41:27 +0700

    Stream Bootcamp

---
+++ app/Models/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transactions';
+
+    protected $fillable = [
+        'package_id', 'user_id', 'amount', 'transaction_code', 'status'
+    ];
+
+    public function package()
+    {
+        return $this->belongsTo(Package::class, 'package_id', 'id'); // foreign key | primary key dari table yg di tuju
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
